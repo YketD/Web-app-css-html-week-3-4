a57fa22889d30ce89dc5c8c6c166f356ac79955c
Yke ten Donkelaar <yketd@hotmail.com>
2016-09-15 20:31:50 +0200

    tried adding image cycler, images added

---
+++ out/artifacts/week_3_4_css_js_web_app_war_exploded/js/imagecycler.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yketd on 15-9-2016.
+ */
+
+
+(function cycler() {
+    imgs = document.getElementById('slider').getElementsByTagName('img')
+    index = 0;
+    imgs[0].style.display = 'block';
+    setInterval(function changeimage() {
+        imgs[index].style.display = 'none';
+        index = (index + 1) % imgs.length;
+        imgs[index].style.display = 'block';
+    }, 5000);
+
+
+
+}());
+
+
