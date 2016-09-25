e503c821e539da08dc1f3e94d5cdd096ebf1e0f4
Marcel Germes <murdock.kaclan@gmail.com>
2016-09-25 12:27:25 +0200

    Contact form validation

--- out/artifacts/week_3_4_css_js_web_app_war_exploded/js/form.js
+++ out/artifacts/week_3_4_css_js_web_app_war_exploded/js/form.js
@@ -1,3 +1,43 @@
 /**
  * Created by Murdock on 25/9/2016.
  */
+function isFieldEmpty(field) {
+    return (field.value == null || field.value == "");
+}
+
+function validateForm() {
+    var result = true;
+    var feedback = "";
+    var form = document.forms["contactform"];
+
+    if (isFieldEmpty(form["firstname"])) {
+        feedback += "Het verplichte veld 'voornaam' bevat geen geldige tekst";
+        result = false;
+    }
+
+    if (isFieldEmpty(form["lastname"])) {
+        feedback += "Het verplichte veld 'achternaam' bevat geen geldige tekst\n";
+        result = false;
+    }
+
+    if (isFieldEmpty(form["email"])) {
+        feedback += "Het verplichte veld 'email' bevat geen geldige tekst\n";
+        result = false;
+    }
+
+    if (isFieldEmpty(form["zip"])) {
+        feedback += "Het verplichte veld 'postcode' bevat geen geldige tekst\n";
+        result = false;
+    }
+
+    if (isFieldEmpty(form["message"])) {
+        feedback += "Het verplichte veld 'bericht' bevat geen geldige tekst\n";
+        result = false;
+    }
+   
+    if (result)
+        feedback = "Dankuwel voor uw bericht, we zullen het zo spoedig mogelijk behandelen";
+
+    alert(feedback);
+    return result;
+}
