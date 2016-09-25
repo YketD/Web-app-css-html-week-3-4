0b8477b1c2ded11785d179df15d9e87ed52342e4
Marcel Germes <murdock.kaclan@gmail.com>
2016-09-25 11:42:24 +0200

    Support for multiple sliders

--- web/js/imagecycler.js
+++ web/js/imagecycler.js
@@ -1,18 +1,31 @@
 /**
  * Created by yketd on 15-9-2016.
  */
-
+var cyclers = [];
 
 (function cycler() {
-    imgs = document.getElementById('slider').getElementsByTagName('img');
-    index = 0;
-    imgs[0].style.display = 'block';
-    setInterval(function changeimage() {
-        imgs[index].style.display = 'none';
-        index = (index + 1) % imgs.length;
-        imgs[index].style.display = 'block';
-    }, 5000);
+    var imgs, index, obj;
+    var sliders = document.getElementsByClassName("slider");
 
+    for (var idx in sliders) {
+        if (!sliders.hasOwnProperty(idx)) continue; // Prototype properties
+        obj = sliders[idx];
+
+        for (var item in obj) {
+            index = 0;
+            imgs = obj.getElementsByTagName("img");
+            cyclers[idx] = {
+                "imageIdx": index,
+                "cycler": item,
+                "images": imgs
+            };
+
+            imgs[0].style.display = "block";
+            setInterval(function (idx) {
+                cyclers[idx].images[index].style.display = "none";
+                index = (index + 1) % cyclers[idx].images.length;
+                cyclers[idx].images[index].style.display = "block";
+            }, 5000, idx);
+        }
+    }
 }());
-
-
