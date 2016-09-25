34fe3855b8ff012f3f12c796e966ae6045197e96
Marcel Germes <murdock.kaclan@gmail.com>
2016-09-25 21:08:51 +0200

    Name fixes and menu bar

--- out/artifacts/week_3_4_css_js_web_app_war_exploded/js/menu.js
+++ out/artifacts/week_3_4_css_js_web_app_war_exploded/js/menu.js
@@ -3,9 +3,9 @@
  */
 function toggleNav() {
     var x = document.getElementById("menu");
-    if (x.className === "menu") {
+    if (x.className === "nav") {
         x.className += " responsive";
     } else {
-        x.className = "menu";
+        x.className = "nav";
     }
 }
